d902c90b0f784da88e0c6553a1b6bb95a3e9735b
mmulekya <kizitohamisi61@gmail.com>
2026-03-18 17:44:41 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,12 @@
-<?php
+<?php<?php
 session_start();
+require_once "includes/config.php";
+require_once "includes/database.php";
 require_once "includes/security.php";
+session_start();
 require_admin();
+
+
 ?>
 
 <!DOCTYPE html>
