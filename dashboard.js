0e2473bfd05fde15e8fb76ebafb04c2d7c22b93c
mmulekya <kizitohamisi61@gmail.com>
2026-03-19 17:52:41 +0300

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,6 +0,0 @@
-async function loadStats() {
-  const res = await fetch("https://yourdomain.com/api/chat_trends.php");
-  const data = await res.json();
-
-  console.log("Top Questions:", data.trends);
-}
