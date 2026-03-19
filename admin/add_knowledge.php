424443ac6d5f940259f089a6c09a629c90b7a5ca
mmulekya <kizitohamisi61@gmail.com>
2026-03-19 12:21:39 +0300

    Create add_knowledge.php

---
+++ admin/add_knowledge.php
@@ -0,0 +1,21 @@
+<?php
+require_once "../../includes/config.php";
+require_once "../../includes/database.php";
+require_once "../../includes/security.php";
+
+session_start();
+require_admin();
+
+$data = json_decode(file_get_contents("php://input"), true);
+$content = sanitize($data['content'] ?? '');
+
+if(!$content){
+    echo json_encode(["error"=>"Empty content"]);
+    exit;
+}
+
+$stmt = $conn->prepare("INSERT INTO knowledge_base (content) VALUES (?)");
+$stmt->bind_param("s", $content);
+$stmt->execute();
+
+echo json_encode(["success"=>"Knowledge added"]);
