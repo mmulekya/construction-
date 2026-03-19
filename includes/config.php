2490c7b1b1ac1d767c0a685115528e3b1fff7425
mmulekya <kizitohamisi61@gmail.com>
2026-03-19 18:05:06 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,9 @@
 <?php
-// Database & general config
-define("DB_HOST", "localhost");
+// DATABASE
+define("DB_HOST", "sqlXXX.infinityfree.com");
 define("DB_USER", "your_db_user");
 define("DB_PASS", "your_db_pass");
 define("DB_NAME", "your_db_name");
 
-// Load environment variables (OpenAI key)
-if(file_exists(__DIR__ . '/../.env')){
-    $env = parse_ini_file(__DIR__ . '/../.env');
-    define("OPENAI_API_KEY", $env['OPENAI_API_KEY']);
-} else {
-    define("OPENAI_API_KEY", "");
-}
+// 🔥 DIRECT API KEY (IMPORTANT for InfinityFree)
+define("OPENAI_API_KEY", "YOUR_OPENAI_API_KEY");
