e3eedd8b07a2102fb9c4820b00b01b3d70dad356
mmulekya <kizitohamisi61@gmail.com>
2026-03-19 19:58:56 +0300

    Create api/create_project.php

---
+++ api/create_project.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../includes/config.php";
+require_once "../includes/database.php";
+require_once "../includes/security.php";
+require_once "../includes/project.php";
+
+session_start();
+require_login();
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$name = sanitize($data['name']);
+$description = sanitize($data['description']);
+
+$id = create_project($conn, $_SESSION['user_id'], $name, $description);
+
+echo json_encode(["success"=>true, "project_id"=>$id]);
