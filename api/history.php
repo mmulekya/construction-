cbb3d8c06eb98f98cac4bc84814b84461322900e
mmulekya <kizitohamisi61@gmail.com>
2026-03-21 13:48:20 +0300

    Update history.php

--- api/history.php
+++ api/history.php
@@ -1,20 +1,24 @@
 <?php
+
 require_once "../includes/config.php";
 require_once "../includes/database.php";
 require_once "../includes/security.php";
 require_once "../includes/history.php";
 
-session_start();
+header("Content-Type: application/json");
+
 require_login();
 
-$user_id = $_SESSION['user_id'];
+// Fetch history
+$result = get_chat_history($conn, $_SESSION['user_id']);
 
-$result = get_user_history($conn, $user_id);
-
-$history = [];
+$data = [];
 
 while($row = $result->fetch_assoc()){
-    $history[] = $row;
+    $data[] = $row;
 }
 
-echo json_encode($history);
+echo json_encode([
+    "success"=>true,
+    "history"=>$data
+]);
