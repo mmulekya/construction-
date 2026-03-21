067a3a772590f7215f86f5d3c5ab3203795e8e63
mmulekya <kizitohamisi61@gmail.com>
2026-03-21 13:39:20 +0300

    Update logout.php

--- api/logout.php
+++ api/logout.php
@@ -1,4 +1,13 @@
 <?php
+
+require_once "../includes/config.php";
+
+header("Content-Type: application/json");
+
 session_start();
+
+// Destroy session completely
+$_SESSION = [];
 session_destroy();
-echo json_encode(["success"=>"Logged out"]);
+
+echo json_encode(["success"=>true]);
