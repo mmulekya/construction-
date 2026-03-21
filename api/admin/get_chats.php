a4a37b579c40944a39b54f58f655bd0d65d58c2a
mmulekya <kizitohamisi61@gmail.com>
2026-03-21 13:55:59 +0300

    Update get_chats.php

--- api/admin/get_chats.php
+++ api/admin/get_chats.php
@@ -1,22 +1,22 @@
 <?php
+
 require_once "../../includes/config.php";
 require_once "../../includes/database.php";
 require_once "../../includes/security.php";
 
-session_start();
+header("Content-Type: application/json");
+
+require_login();
 require_admin();
 
 $result = $conn->query("
-    SELECT users.username, chat_history.question, chat_history.answer, chat_history.created_at
-    FROM chat_history
-    JOIN users ON users.id = chat_history.user_id
-    ORDER BY chat_history.id DESC
-    LIMIT 100
+    SELECT * FROM chat_history ORDER BY created_at DESC LIMIT 100
 ");
 
-$data = [];
+$chats = [];
+
 while($row = $result->fetch_assoc()){
-    $data[] = $row;
+    $chats[] = $row;
 }
 
-echo json_encode($data);
+echo json_encode(["success"=>true, "chats"=>$chats]);
