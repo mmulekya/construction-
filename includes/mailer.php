5ca01078b8d2687180eef8d6f2a57f00996a173d
mmulekya <kizitohamisi61@gmail.com>
2026-03-22 13:07:58 +0300

    Create includes/mailer.php

---
+++ includes/mailer.php
@@ -0,0 +1,10 @@
+<?php
+
+function send_email($to, $subject, $message){
+
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: no-reply@yourdomain.com";
+
+    return mail($to, $subject, $message, $headers);
+}
