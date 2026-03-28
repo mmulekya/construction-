30d3fdd3a279f77dc749c0d58cf8fb943fcf1a3f
mmulekya <kizitohamisi61@gmail.com>
2026-03-28 15:05:31 +0300

    Create api/admin/backup_db.php

---
+++ api/admin/backup_db.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "../../includes/config.php";
+require_once "../../includes/database.php";
+require_once "../../includes/security.php";
+
+header("Content-Type: application/json");
+
+require_login();
+require_admin();
+
+// Tables to backup
+$tables = [
+    "users",
+    "chat_messages",
+    "projects",
+    "project_data",
+    "pdf_documents",
+    "pdf_chunks"
+];
+
+$output = "";
+
+foreach($tables as $table){
+
+    $result = $conn->query("SELECT * FROM $table");
+
+    while($row = $result->fetch_assoc()){
+        $columns = implode("`, `", array_keys($row));
+        $values = implode("', '", array_map([$conn,'real_escape_string'], $row));
+
+        $output .= "INSERT INTO `$table` (`$columns`) VALUES ('$values');\n";
+    }
+}
+
+// Download file
+header('Content-Type: application/sql');
+header('Content-Disposition: attachment; filename=backup.sql');
+
+echo $output;
+exit;
