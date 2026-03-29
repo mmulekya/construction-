00b16da677a0457b3db5de6a016eb6b699907a11
mmulekya <kizitohamisi61@gmail.com>
2026-03-29 13:50:15 +0300

    Update delete_chat.php

--- api/admin/delete_chat.php
+++ api/admin/delete_chat.php
@@ -1,23 +1,17 @@
 <?php
-
 require_once "../../includes/config.php";
 require_once "../../includes/database.php";
 require_once "../../includes/security.php";
 
 header("Content-Type: application/json");
-
+session_start();
 require_login();
 require_admin();
 
-// CSRF
-if(!verify_csrf_token($_POST['csrf_token'] ?? '')){
-    exit(json_encode(["error"=>"Invalid CSRF"]));
-}
+$id = intval($_POST['id']);
 
-$id = intval($_POST['id'] ?? 0);
-
-$stmt = $conn->prepare("DELETE FROM chat_history WHERE id=?");
-$stmt->bind_param("i", $id);
+$stmt = db_prepare("DELETE FROM chat_messages WHERE id=?");
+$stmt->bind_param("i",$id);
 $stmt->execute();
 
 echo json_encode(["success"=>true]);
